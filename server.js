42404d8c0cbc98717d81d488b57cadbfb6cde378
rofiqirapsanjani <achmad.rofiqi@ui.ac.id>
2022-07-14 23:09:37 +0700

    add request path

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello rofiqi');
+  res.send('Hello rapsanjani');
 });
+app.get('/api/', (req, res) => {
+  res.json("kamu hebat");
+});
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
